fc58410309a3757fcc67230e9aa5782e5e45ce4c
Peter Sammon <petersammon2@gmail.com>
2020-06-13 18:09:56 -0400

    added sudo comments

---
+++ dist/js/main.js
@@ -0,0 +1,22 @@
+//*****************************FRONT END**************************************** */
+// navbar with a search and a logo
+// search links to mapping/results page 
+// home page
+// img spans for outer borders
+// border animations at 12s for now
+// main jumbotron/card
+//  - expand when hovered
+//  - back to original size when active
+//  - enter area credendtials (zip code, current city)
+//  - when submit is clicked on:
+//      - mapping/results page displays
+//      - map of your current area with 25 miles on the right section
+//      - another search input for type of food interest/ type of resturant on the left
+//      - when submit is clicked, the form input hides, and display a list of resurants 
+//      - on the right where the map is diplayed, when the user hovers over the map where place of food is located, a modal appears displaying:
+//          - name of resturant
+//          - reviews of resturant
+//          - location
+//  sticky footer on both pages
+// 
+//  
