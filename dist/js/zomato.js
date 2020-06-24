f977cc971fd40207768ee6a5626253b711a1452d
Peter Sammon <petersammon2@gmail.com>
2020-06-24 02:22:49 -0400

    initial commit

--- dist/js/zomato.js
+++ dist/js/zomato.js
@@ -1,30 +1,4 @@
-
-
-//*****************************FRONT 
-// END**************************************** */
-// navbar with a search and a logo
-// search links to mapping/results page 
-// home page
-// img spans for outer borders
-// border animations at 12s for now
-// main jumbotron/card
-//  - expand when hovered
-//  - back to original size when active
-//  - enter area credendtials (zip code, current city)
-//  - when submit is clicked on:
-//      - mapping/results page displays
-//      - map of your current area with 25 miles on the right section
-//      - another search input for type of food interest/ type of resturant on the left
-//      - when submit is clicked, the form input hides, and display a list of resurants 
-//      - on the right where the map is diplayed, when the user hovers over the map where place of food is located, a modal appears displaying:
-//          - name of resturant
-//          - reviews of resturant
-//          - location
 document.getElementById('search').addEventListener('click', getResults);
-document.getElementById('search').addEventListener('click', displayMap);
-
-
-
 function getResults() {
     $.getJSON('https://ipapi.co/json/', (res) => {
         console.log(res);
@@ -38,7 +12,8 @@ function getResults() {
             })
         })
             .then((data) => {
-                function myMap() {
+                const restaurants = data.restaurants
+                const myMap = function () {
                     var mapProp = {
                         center: new google.maps.LatLng(
                             restaurants[0].restaurant.location.latitude,
@@ -46,12 +21,10 @@ function getResults() {
                         ),
                         zoom: 15,
                     };
-
                     var map = new google.maps.Map(
                         document.getElementById('googleMap'),
                         mapProp
                     );
-
                     for (i = 0; i < restaurants.length; i++) {
                         marker = new google.maps.Marker({
                             position: new google.maps.LatLng(
@@ -62,12 +35,6 @@ function getResults() {
                         });
                     }
                 }
-                myMap();
-                console.log(data);
-
-                const restaurants = data.restaurants[0].restaurant.name;
-
-
                 let output = '<h2>Restaurants</h2>';
                 for (let i = 0; i < restaurants.length; i++) {
                     output += `
@@ -106,30 +73,3 @@ function getResults() {
             })
     })
 }
-
-function displayMap() {
-    $.ajax({
-        method: "GET",
-        url: `https://maps.googleapis.com/maps/api/js`,
-        dataType: "json",
-        async: true,
-        beforeSend: ((xhr) => {
-            xhr.setRequestHeader("key", "AIzaSyBCsWd-NAiAybduTcHpH46UpHr1_1GxX4w");
-        })
-    })
-}
-
-
-
-
-
-
-
-
-
-
-// "zomatoUrl" is the url we'll use to query the API
-// begin building an object to contain our API's call's query parameters
-// set the API key
-// grab text the user typed into the search input, add to "zomatoObject"
-// if the user provides a type of resturant include it in the "zomatoObject"
